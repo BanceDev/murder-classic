43e56237b1a07d02ecf48d27c1c9b75115ecae0c
BanceDev <lanceaborden@gmail.com>
2024-01-21 22:02:23 -0500

    i better fix this frfr

--- dlls/multiplay_gamerules.cpp
+++ dlls/multiplay_gamerules.cpp
@@ -1409,130 +1409,19 @@ Server is changing to a new level, check mapcycle.txt for map name and setup inf
 */
 void CHalfLifeMultiplay::ChangeLevel()
 {
-	static char szPreviousMapCycleFile[256];
-	static mapcycle_t mapcycle;
-
-	char szNextMap[32];
-	char szFirstMapInList[32];
-	char szCommands[1500];
-	char szRules[1500];
-	int minplayers = 0, maxplayers = 0;
-	strcpy(szFirstMapInList, "hldm1"); // the absolute default level is hldm1
-
-	int curplayers;
-	bool do_cycle = true;
-
-	// find the map to change to
-	char* mapcfile = (char*)CVAR_GET_STRING("mapcyclefile");
-	ASSERT(mapcfile != NULL);
-
-	szCommands[0] = '\0';
-	szRules[0] = '\0';
-
-	curplayers = CountPlayers();
-
-	// Has the map cycle filename changed?
-	if (stricmp(mapcfile, szPreviousMapCycleFile))
-	{
-		strcpy(szPreviousMapCycleFile, mapcfile);
-
-		DestroyMapCycle(&mapcycle);
-
-		if (!ReloadMapCycleFile(mapcfile, &mapcycle) || (!mapcycle.items))
-		{
-			ALERT(at_console, "Unable to load map cycle file %s\n", mapcfile);
-			do_cycle = false;
+	// setup roles
+	m_iInGame = false;
+	for (int i = 1; i <= m_iClients; i++) {
+		CBasePlayer* pPlayer = (CBasePlayer*)(UTIL_PlayerByIndex(i));
+		if (pPlayer && pPlayer->IsPlayer()) {
+			pPlayer->StopObserver();
+			pPlayer->m_iPlayerRole = -1;
+			pPlayer->Spawn();
+			MESSAGE_BEGIN(MSG_ONE, gmsgRole, NULL, pPlayer->pev);
+			WRITE_BYTE(5);
+			MESSAGE_END();
 		}
 	}
-
-	if (do_cycle && mapcycle.items)
-	{
-		bool keeplooking = false;
-		bool found = false;
-		mapcycle_item_s* item;
-
-		// Assume current map
-		strcpy(szNextMap, STRING(gpGlobals->mapname));
-		strcpy(szFirstMapInList, STRING(gpGlobals->mapname));
-
-		// Traverse list
-		for (item = mapcycle.next_item; item->next != mapcycle.next_item; item = item->next)
-		{
-			keeplooking = false;
-
-			ASSERT(item != NULL);
-
-			if (item->minplayers != 0)
-			{
-				if (curplayers >= item->minplayers)
-				{
-					found = true;
-					minplayers = item->minplayers;
-				}
-				else
-				{
-					keeplooking = true;
-				}
-			}
-
-			if (item->maxplayers != 0)
-			{
-				if (curplayers <= item->maxplayers)
-				{
-					found = true;
-					maxplayers = item->maxplayers;
-				}
-				else
-				{
-					keeplooking = true;
-				}
-			}
-
-			if (keeplooking)
-				continue;
-
-			found = true;
-			break;
-		}
-
-		if (!found)
-		{
-			item = mapcycle.next_item;
-		}
-
-		// Increment next item pointer
-		mapcycle.next_item = item->next;
-
-		// Perform logic on current item
-		strcpy(szNextMap, item->mapname);
-
-		ExtractCommandString(item->rulebuffer, szCommands);
-		strcpy(szRules, item->rulebuffer);
-	}
-
-	if (!IS_MAP_VALID(szNextMap))
-	{
-		strcpy(szNextMap, szFirstMapInList);
-	}
-
-	g_fGameOver = true;
-
-	ALERT(at_console, "CHANGE LEVEL: %s\n", szNextMap);
-	if (0 != minplayers || 0 != maxplayers)
-	{
-		ALERT(at_console, "PLAYER COUNT:  min %i max %i current %i\n", minplayers, maxplayers, curplayers);
-	}
-	if (strlen(szRules) > 0)
-	{
-		ALERT(at_console, "RULES:  %s\n", szRules);
-	}
-
-	CHANGE_LEVEL(szNextMap, NULL);
-	if (strlen(szCommands) > 0)
-	{
-		SERVER_COMMAND(szCommands);
-	}
-	
 }
 
 #define MAX_MOTD_CHUNK 60
